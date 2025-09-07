18421fa38c4d3324d69172da7ab1508799adce41
Victor <victormcdantas@gmail.com>
2025-09-07 20:28:42 -0300

    Site base

--- my-site/app/page.js
+++ my-site/app/page.js
@@ -1,53 +1,95 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
 export default function Home() {
   return (
-    <div>
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol>
+          <li>
+            Get started by editing <code>app/page.js</code>.
+          </li>
+          <li>Save and see your changes instantly.</li>
+        </ol>
 
-      <header>
-        <nav>
-          <h1>Meu Portfólio</h1>
-          <ul>
-            <li><a href="#hero">Início</a></li>
-            <li><a href="#about">Sobre</a></li>
-            <li><a href="#contact">Contato</a></li>
-          </ul>
-        </nav>
-      </header>
-
-      
-      <section id="hero">
-        <h1>Olá, meu nome é Victor</h1>
-        <p>Boas vindas ao meu portfólio!</p>
-      </section>
-
-      
-      <section id="about">
-        <h2>Sobre Mim</h2>
-        <div className="about-content">
-          <div className="about-text">
-            <p>Atualmente tenho 23 anos, sou de Teresina-PI, curso Ciência da Computação desde 2022 e gosto de várias coisas e falar sobre elas</p>
-            <p>Gosto muito de filmes, jogos, quadrinhos e animações. Tenho sonho de conhecer diferentes países e ajudar pessoas. Profissionalmente estou aberto a qualquer tipo de experiência.</p>
-          </div>
+        <div className={styles.ctas}>
+          <a
+            className={styles.primary}
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className={styles.logo}
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.secondary}
+          >
+            Read our docs
+          </a>
         </div>
-      </section>
-
-      
-      <section id="contact">
-        <h2>Contato</h2>
-        <div className="contact-links">
-          <div className="contact-item">
-            <span>📧</span>
-            <a href="mailto:victormcdantas@gmail.com">victormcdantas@gmail.com</a>
-          </div>
-          <div className="contact-item">
-            <span>💼</span>
-            <a href="https://www.linkedin.com/in/victormcdantas/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-          </div>
-          <div className="contact-item">
-            <span>🐙</span>
-            <a href="https://github.com/victormcdantas02" target="_blank" rel="noopener noreferrer">GitHub</a>
-          </div>
-        </div>
-      </section>
+      </main>
+      <footer className={styles.footer}>
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
     </div>
   );
 }
